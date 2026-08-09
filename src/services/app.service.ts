92d8991ea50c366deec20c30530e9ce23509af47
Presab Khadka <presabkhadka30@gmail.com>
2026-08-09 12:56:12 +0545

    feat: file upload, share, and download lgoic

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,11 +1,55 @@
 import fs from "node:fs/promises"
 import path from "node:path"
+import { redis } from "../database/redis.database.ts"
+import { randomUUID } from "node:crypto"
 
-const saveFile = async (file: File) => {
-  const uploadPath = path.join('storage', file.name)
-  await fs.writeFile(uploadPath, Buffer.from(await file.arrayBuffer()), 'utf-8')
+interface mime {
+  originalFilename: string,
+  storedFilename: string,
+  mimeType: string,
+  size: number,
+  path: string,
+  downloadCount: number,
+  createdAt: number,
+  expiresAt: number
 }
 
-export const shareFile = async (file: File) => {
-  saveFile(file)
+export const saveAndShareFile = async (file: File) => {
+  const storedFilename = randomUUID() + file.name
+  const uploadPath = path.join('storage', storedFilename)
+  await fs.writeFile(uploadPath, Buffer.from(await file.arrayBuffer()))
+
+  const fileDetails: mime = {
+    originalFilename: file.name,
+    storedFilename,
+    mimeType: file.type,
+    size: file.size,
+    path: uploadPath,
+    downloadCount: 0,
+    createdAt: Date.now(),
+    expiresAt: Date.now() + 60 * 60
+  }
+
+  const uuid = randomUUID()
+
+  await redis.set(`file:${uuid}`, JSON.stringify(fileDetails), { expiration: { type: "EX", value: 60 * 60 } })
+
+  return {
+    cacheKey: `file:${uuid}`
+  }
+}
+
+export const downloaadFile = async (fileKeys: string) => {
+  if (!fileKeys) throw new Error('Filekey is required')
+
+  const value = await redis.get(fileKeys)
+
+  if (!value) throw new Error('Invalid cache key')
+  console.log("value", value)
+
+  const parsedValue: mime = JSON.parse(value)
+
+  const filePath = path.join("storage", parsedValue.storedFilename)
+  const file = await fs.readFile(filePath)
+  return file
 }
